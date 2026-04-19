a10422c408ce890d0c9fcef71444377af76382e1
ali200010 <zx.tyk2000@gmail.com>
2026-04-20 01:50:46 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,18 +2,19 @@ import telebot
 import yt_dlp
 import os
 
+# Updated Token from your BotFather screenshot
 BOT_TOKEN = '8129938298:AAEC2a-d6baYFWnPTyrnKvcsCeYDKSmcNJK'
 bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Welcome! Send me a link to download.")
+    bot.reply_to(message, "Welcome! Send me a TikTok or YouTube link.")
 
 @bot.message_handler(func=lambda message: True)
 def download_video(message):
     url = message.text
     if "http" in url:
-        msg = bot.reply_to(message, "Wait... Downloading...")
+        msg = bot.reply_to(message, "Downloading... Please wait.")
         try:
             ydl_opts = {
                 'format': 'best',
@@ -31,6 +32,7 @@ def download_video(message):
         except Exception as e:
             bot.edit_message_text("Error in download!", message.chat.id, msg.message_id)
     else:
-        bot.reply_to(message, "Please send a valid link.")
+        bot.reply_to(message, "Invalid link.")
 
 bot.polling()
+
