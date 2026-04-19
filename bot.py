b7d1ad44748035a8300ce4fe5aaa58386adfd8fb
ali200010 <zx.tyk2000@gmail.com>
2026-04-20 02:17:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,18 +2,19 @@ import telebot
 import yt_dlp
 import os
 
+# Authorized Token for @Alieasl2000bot
 BOT_TOKEN = '8129938298:AAEC2a-d6baYFWnPTyrnKvcsCeYDKSmcNJK'
 bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Welcome! Send link to download.")
+    bot.reply_to(message, "Bot is Active. Send a link to download.")
 
 @bot.message_handler(func=lambda message: True)
 def download_video(message):
     url = message.text
     if "http" in url:
-        msg = bot.reply_to(message, "Downloading... wait.")
+        msg = bot.reply_to(message, "Processing link...")
         try:
             ydl_opts = {
                 'format': 'best',
@@ -27,11 +28,13 @@ def download_video(message):
             with open('video.mp4', 'rb') as video:
                 bot.send_video(message.chat.id, video)
             
-            os.remove('video.mp4')
+            if os.path.exists('video.mp4'):
+                os.remove('video.mp4')
             bot.delete_message(message.chat.id, msg.message_id)
         except Exception as e:
             bot.edit_message_text("Download Error!", message.chat.id, msg.message_id)
     else:
-        bot.reply_to(message, "Invalid Link.")
+        bot.reply_to(message, "Please send a valid URL.")
 
-bot.polling()
+if __name__ == "__main__":
+    bot.polling(none_stop=True)
