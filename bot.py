7ab85e941906454fb771d75d40d06d18f653b0bf
ali200010 <zx.tyk2000@gmail.com>
2026-04-20 12:00:39 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,19 +14,19 @@ def run_flask():
     port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
 
-# Token from your latest screenshot with no spaces
-BOT_TOKEN = '8129938298:AAFNAOIVq9NUUtfU9EN3Zpwv4dJVdU-cP-Y'
+# The NEW Token from your latest screenshot (11:57 AM)
+BOT_TOKEN = '8129938298:AAGDQbxHy81J1mtyjV1NPWx2w-t-HuJqD5k'
 bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Bot Active. Send link.")
+    bot.reply_to(message, "Bot Active. Send me a link to download.")
 
 @bot.message_handler(func=lambda message: True)
 def download_video(message):
     url = message.text
     if "http" in url:
-        msg = bot.reply_to(message, "Downloading...")
+        msg = bot.reply_to(message, "Downloading... Please wait.")
         try:
             ydl_opts = {
                 'format': 'best',
@@ -44,11 +44,12 @@ def download_video(message):
             
             bot.delete_message(message.chat.id, msg.message_id)
         except Exception as e:
-            bot.edit_message_text("Error!", message.chat.id, msg.message_id)
+            bot.edit_message_text("Download Error!", message.chat.id, msg.message_id)
     else:
-        bot.reply_to(message, "Invalid URL.")
+        bot.reply_to(message, "Please send a valid link.")
 
 if __name__ == "__main__":
     t = Thread(target=run_flask)
     t.start()
     bot.polling(none_stop=True)
+
